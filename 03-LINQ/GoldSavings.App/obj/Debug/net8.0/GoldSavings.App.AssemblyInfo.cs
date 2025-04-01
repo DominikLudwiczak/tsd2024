5cb660d3b5c4bd623b6d633e14940765485aa641
DominikLudwiczak <dominik.ludwiczak.priv@gmail.com>
2025-04-01 19:35:34 +0200

    idk

--- 03-LINQ/GoldSavings.App/obj/Debug/net8.0/GoldSavings.App.AssemblyInfo.cs
+++ 03-LINQ/GoldSavings.App/obj/Debug/net8.0/GoldSavings.App.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GoldSavings.App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c1683e5a402e6843e6a14f48161b1c7f965c049")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5802f9d8245f7fa3e96a1404a3113da1e8e6f9b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("GoldSavings.App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GoldSavings.App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
